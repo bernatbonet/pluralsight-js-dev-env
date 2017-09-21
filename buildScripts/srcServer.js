48be2bf7a6ba79fd6001287394a529c29b4a0267
bernatbonet <bernatbonet@gmail.com>
2017-09-21 12:57:55 +0200

    bundler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,16 @@
-var express = require('express');
+// ES5
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 8009;
+const app = express();
+/*var express = require('express');
 var path = require('path');
 var open = require('open');
 
 var port = 8009;
-var app = express();
+var app = express();*/
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
-var chalk = require('chalk');
+import chalk from 'chalk'; // ES5
+//var chalk = require('chalk');
 
 console.log(chalk.green('Starting app in dev mode...'));
